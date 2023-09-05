227b77ad175477b832e800434878a475c181f49f
Tevin Aduma <tevin74@live.com>
2023-09-05 20:04:03 +0300

    static function to pull similar posts sharing category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public static function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
