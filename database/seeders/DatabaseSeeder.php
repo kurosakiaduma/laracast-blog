f9b421d1da3a9985e971f089d85aecb767fe0c77
Tevin Aduma <tevin74@live.com>
2023-09-05 22:45:05 +0300

    seed the db with fake users, posts and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,85 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Category::create([
+            'name'=>"Personal",
+            'slug'=>"personal"
+        ]);
+
+        Category::create([
+            'name'=>"Work",
+            'slug'=>"work"
+        ]);
+
+        Category::create([
+            'name'=>"Hobby",
+            'slug'=>"hobby"
+        ]);
+
+        Post::create([
+            'category_id'=>'1',
+            'user_id'=>'1',
+            'title'=>'My First Story',
+            'excerpt'=>'Excerpt for the first story',
+            'body'=>'<p>Lorem ipsum dolar sit amet</p>',
+            'author'=>'Tevin Aduma',
+            'slug'=>'my-first-post',
+        ]);
+
+
+        Post::create([
+            'category_id'=>'1',
+            'user_id'=>'2',
+            'title'=>'My Second Story',
+            'excerpt'=>'Excerpt for the second story',
+            'body'=>'<p>Lorem ipsum dolar sit amet</p>',
+            'author'=>'Jane Aduma',
+            'slug'=>'my-second-post',
+        ]);
+
+
+        Post::create([
+            'category_id'=>'2',
+            'user_id'=>'1',
+            'title'=>'My Hobby Story',
+            'excerpt'=>'Excerpt for the hobby story',
+            'body'=>'<p>Lorem ipsum dolar sit amet</p>',
+            'author'=>'Tevin Aduma',
+            'slug'=>'my-hobby-post',
+        ]);
+
+
+        Post::create([
+            'category_id'=>'3',
+            'user_id'=>'2',
+            'title'=>'My Fourth Story',
+            'excerpt'=>'Excerpt for the second story',
+            'body'=>'<p>Lorem ipsum dolar sit amet</p>',
+            'author'=>'Jane Aduma',
+            'slug'=>'my-fourth-post',
+        ]);
+
+
+        Post::create([
+            'category_id'=>'2',
+            'user_id'=>'3',
+            'title'=>'My Work Story',
+            'excerpt'=>'Excerpt for the work story',
+            'body'=>'<p>Lorem ipsum dolar sit amet</p>',
+            'author'=>'Worker Aduma',
+            'slug'=>'my-worker-post',
+        ]);
     }
 }
