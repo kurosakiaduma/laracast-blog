9b6c84114600d9b78b8aa4d73b4cd144c89bc812
Tevin Aduma <tevin74@live.com>
2023-09-05 16:47:01 +0300

    included category in the individual post page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,9 +10,17 @@ class Post extends Model
     use HasFactory;
 
     /*Mass assignment fields*/
-    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'author'];
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'author', 'category_id'];
 
     /* Protected fields from mass assignment to avoid vulnerabilities*/
     protected $guarded = ['id'];
+
+    public function category()
+    {
+        #LARAVEL's relationship types
+        # hasOne, hasMany, belongsTo, belongsToMany
+        return $this->belongsTo(Category::class);
+
+    }
 
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="content">
         <article>
             <h1 class='text-center'>{{ $post->title }}</h1>
+            <h4><strong>Category: </strong>{{ $post->category->name }}</h4>
             <h4><strong>Author: </strong>{{ $post->author }}</h4>
             <h4><strong>Date Published: </strong>{{ $post->date_published }}</h4>
             <div> {!! $post->body !!}</div>
