015de22d670b174e27882b6cdcefcb51e00f9d35
Tevin Aduma <tevin74@live.com>
2023-09-08 01:22:51 +0300

    extract article and headers into separate components

---
+++ resources/views/components/post-card.blade.php
@@ -0,0 +1,41 @@
+<article
+    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    <div class="py-6 px-5">
+        <div class="lg:mr-8 lg:mt-5 flex">
+            <img src="/images/illustration-5.png" alt="Blog Post illustration" class="rounded-xl">
+        </div>
+
+        <div class="mt-8 flex flex-col justify-between">
+
+            <div class="text-sm mt-4">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+
+                <p class="mt-4">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+
+            <footer class="flex justify-between items-center mt-8">
+                <div class="flex items-center text-sm">
+                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <div class="ml-3">
+                        <h5 class="font-bold">Lary Laracore</h5>
+                        <h6>Mascot at Laracasts</h6>
+                    </div>
+                </div>
+
+                <div>
+                    <a href="#"
+                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                    >
+                        Read More
+                    </a>
+                </div>
+            </footer>
+        </div>
+    </div>
+</article>
