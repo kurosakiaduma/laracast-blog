c3d584e200d0485e50364796db5c8606cb966ea0
Tevin Aduma <tevin74@live.com>
2023-09-11 14:26:16 +0300

    update settings to display links conditionally

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -10,13 +10,25 @@
             <h4 class="font-semibold mb-4">Links</h4>
 
             <ul>
-                <li>
-                    <a href="/admin/posts" class="{{ request()->is('admin/posts') ? 'text-blue-500' : '' }}">All Posts</a>
-                </li>
+                @if ($heading === 'Manage Posts')
+                    <a href="/admin/posts" class="{{ request()->is('admin/posts') ? 'text-blue-500' : '' }}">
+                        All Posts
+                        <br>
+                    </a>
+                    <a href="/admin/posts/create" class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : '' }}">
+                        New Post
+                    </a>
+                @endif
 
-                <li>
-                    <a href="/admin/posts/create" class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : '' }}">New Post</a>
-                </li>
+                @if ($heading === 'User Management')
+                    <a href="/admin/users" class="{{ request()->is('admin/users') ? 'text-blue-500' : '' }}">
+                        All Users
+                    </a>
+                    <br>
+                    <a href="/admin/users/create" :active=class="{{ request()->is('admin/users/create') ? 'text-blue-500' : '' }}">
+                        New User
+                    </a>
+                @endif
             </ul>
         </aside>
 
