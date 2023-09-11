cce84cc14f9fe605f8cf0e909c1730c2a849528c
Tevin Aduma <tevin74@live.com>
2023-09-11 12:02:30 +0300

    implement method to retrieve all users through controller

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -1,10 +1,18 @@
 <?php
 
+// app/Http/Controllers/AdminUserController.php
+
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
-    //
+    public function show(User $user)
+    {
+        $roles = $user->getRoles(); // This ports from the getRoles() method in User model
+
+        return view('admin.users.show', compact('user', 'roles'));
+    }
 }
