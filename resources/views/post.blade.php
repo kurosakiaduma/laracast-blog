99df59d9efcf90a1f83ecbb28556dd5c4cc38312
Tevin Aduma <tevin74@live.com>
2023-09-12 10:17:13 +0300

    fix issues with image ref

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,10 +6,10 @@
         <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                    <img src="/images/illustration-1.png" alt="" class="rounded-xl">
+                    <img src="images/illustration-1.png" alt="" class="rounded-xl">
 
                     <div class="flex items-center lg:justify-center text-sm mt-4">
-                        <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                        <img src="images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
                             <h5 class="font-bold"> <a href="authors/{{ $post->author->username }}">{{ $post->author->name }}</a></h5>
                         </div>
