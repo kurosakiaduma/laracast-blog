45c6ebd55bf6b32576e2112d7f807fe89cfbfc99
Tevin Aduma <tevin74@live.com>
2023-09-26 03:39:52 +0300

    feat: Update HomeView and App.vue components
    - Made improvements to frontend/src/views/HomeView.vue
    - Addressed line ending warnings in frontend/src/App.vue
    
    This commit enhances the HomeView component and resolves line ending issues in App.vue.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,41 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+        <Header/>
+
+        <main class="max-w-6xl mx-autoclassName lg:mt-20 space-y-6">
+            <div v-if="posts.length > 0">
+                <PostsGrid :posts="posts"/>
+
+                <!-- Pagination component goes here -->
+            </div>
+            <p v-else class="text-center">No posts yet ☹️. Please check back later.</p>
+        </main>
 </template>
+
+<script>
+import axios from 'axios';
+import PostsGrid from "@/components/common/PostsGrid.vue";
+import Header from "@/components/common/Header.vue";
+
+export default {
+    components: {Header, PostsGrid},
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    mounted() {
+        // Make an API request to fetch posts from your Laravel backend
+        this.fetchPosts();
+    },
+    methods: {
+        async fetchPosts() {
+            try {
+                const response = await axios.get('http://localhost:8000/api/posts'); // Adjust the API endpoint as needed
+                this.posts = response.data.posts.data;
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        },
+    },
+};
+</script>
