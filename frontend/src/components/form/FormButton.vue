2bcb8289c207ae73a2c07ca59d2481dd5792d1c8
Tevin Aduma <tevin74@live.com>
2023-09-26 11:23:33 +0300

    transitioned form components to Vue

---
+++ frontend/src/components/form/FormButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <FormField>
+        <button
+            type="submit"
+            class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
+        >
+            <slot></slot>
+        </button>
+        </FormField>
+    </div>
+</template>
+
+<script>
+import FormField from "@/components/form/FormField.vue";
+
+export default {
+    name: 'FormButton',
+    components: {
+        FormField,
+    },
+};
+</script>
---
+++ frontend/src/components/form/FormTextarea.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <label :for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+            {{ label }}
+        </label>
+
+        <textarea
+            class="border border-gray-200 p-2 w-full rounded"
+            :name="name"
+            :id="name"
+            required
+            v-bind="$attrs"
+            v-model="value"
+        ></textarea>
+
+        <FormError :name="name" />
+
+    </div>
+</template>
+
+<script>
+import FormError from './FormError.vue';
+
+export default {
+    name: "FormTextarea",
+    components: {
+        FormError,
+    },
+    props: {
+        name: String,
+        label: String,
+        value: String,
+    },
+};
+</script>
