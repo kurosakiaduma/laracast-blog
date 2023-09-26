94abc1fbafa5da62dd28a7a40891080bff175638
Tevin Aduma <tevin74@live.com>
2023-09-26 11:35:45 +0300

    update routes in Vue Routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Layout from '../components/Layout.vue'
+import HomeView from '../views/HomeView.vue';
+import RegisterView from "@/views/RegisterView.vue";
+import SessionCreate from "@/views/SessionCreate.vue";
+import { defineAsyncComponent } from 'vue'
+
+const PostView = ()=> import('@/views/PostView.vue')
+
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/posts/:slug', //  dynamic route parameter ":slug"
+            name: 'post',
+            component: PostView, // PostView component for the "/posts/:slug" route
+            props: true // Enable passing route params as props to the component
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: SessionCreate
+        }
+
+    ]
 })
 
 export default router
