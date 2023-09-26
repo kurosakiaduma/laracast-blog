50f4bd694711290343362cc571daaf85cea3e3cd
Tevin Aduma <tevin74@live.com>
2023-09-26 16:07:59 +0300

    feat: Add API endpoint for saving posts from PostCreate.vue
    Description:
    - Added a new API endpoint in `app/Http/Controllers/PostController.php` to handle the saving of posts from the frontend.
    - The API endpoint uses a randomizer to set the author and post_id for the newly created post.
    
    Changes Made:
    - Added a new method in `PostController.php` to handle post creation via API.
    - Implemented logic to set author and post_id using a randomizer.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
+
 
 class PostController extends Controller
 {
@@ -21,5 +23,32 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return response()->json(['post' => $post]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts',
+            'thumbnail' => 'required|image',
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $thumbnail = $request->file('thumbnail')->store('thumbnails');
+
+        $post = Post::create([
+            'user_id' => rand(1, 7), // Replace with auth()->id() if you want to use the authenticated user's ID
+            'category_id' => $request->category_id,
+            'slug' => $request->slug,
+            'title' => $request->title,
+            'thumbnail' => $thumbnail,
+            'excerpt' => $request->excerpt,
+            'body' => $request->body,
+            'status' => 'published',
+        ]);
+
+        return response()->json($post, 201);
     }
 }
