2410bb0d230873b1b45e88c78be1e53c05fb2ba6
Tevin Aduma <tevin74@live.com>
2023-09-26 11:34:47 +0300

    modify session/register controllers to return json responses

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,16 +19,14 @@ class SessionsController extends Controller
         ]);
 
         if (! auth()->attempt($attributes)) {
-            throw ValidationException::withMessages([
-                'email' => 'Your provided credentials could not be verified.'
-            ]);
+            return response()->json(['message' => 'Your provided credentials could not be verified.'], 401);
         }
 
-        //Done to alter the session id to prevent hackers from taking over user logins
         session()->regenerate();
 
-        return redirect('/')->with('success', 'Welcome Back!');
+        return response()->json(['message' => 'Welcome Back!']);
     }
+
 
     public function destroy()
     {
