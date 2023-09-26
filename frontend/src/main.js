954fdd4bfc744798f243e47e5ceea6f91c406bf6
Tevin Aduma <tevin74@live.com>
2023-09-26 11:34:10 +0300

    convert PostView into an asyncComp

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import store from './stores/store'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
