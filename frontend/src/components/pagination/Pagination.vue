ffecc67c0ec8b1c421d3cef8e8278e1b734e4dd2
Tevin Aduma <tevin74@live.com>
2023-09-26 04:38:11 +0300

    altered pagination

---
+++ frontend/src/components/pagination/Pagination.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+    props: {
+        pagination: Object,
+    },
+    methods: {
+        changePage(page) {
+            this.$emit('page-change', page);
+        },
+    },
+};
+</script>
+
+<template>
+    <nav>
+        <ul class="pagination">
+            <li class="page-item" v-if="pagination.prev_page_url">
+                <a class="page-link" @click="changePage(pagination.current_page - 1)" href="#">
+                    Previous
+                </a>
+            </li>
+            <li class="page-item" v-for="page in pagination.last_page" :key="page">
+                <a class="page-link" @click="changePage(page)" href="#" :class="{ 'active': page === pagination.current_page }">
+                    {{ page }}
+                </a>
+            </li>
+            <li class="page-item" v-if="pagination.next_page_url">
+                <a class="page-link" @click="changePage(pagination.current_page + 1)" href="#">
+                    Next
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+.pagination {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.page-item {
+    margin: 0 5px;
+}
+
+.page-link {
+    display: inline-block;
+    padding: 8px 12px;
+    text-align: center;
+    text-decoration: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background-color: #fff;
+    color: #333;
+    cursor: pointer;
+}
+
+.active {
+    background-color: #007bff;
+    color: #fff;
+    border: 1px solid #007bff;
+}
+
+.page-link.active {
+    background-color: #007bff;
+    color: #fff;
+    border: 1px solid #007bff;
+}
+
+.page-link:hover {
+    background-color: #ccc;
+}
+
+.page-link[disabled] {
+    cursor: not-allowed;
+    pointer-events: none;
+    background-color: #eee;
+    color: #999;
+    border: 1px solid #ccc;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,9 +3,8 @@
 
         <main class="max-w-6xl mx-autoclassName lg:mt-20 space-y-6">
             <div v-if="posts.length > 0">
-                <PostsGrid :posts="posts"/>
-
-                <!-- Pagination component goes here -->
+                <PostsGrid :posts="posts" />
+                <Pagination :pagination="pagination" @page-change="fetchPosts" />
             </div>
             <p v-else class="text-center">No posts yet ☹️. Please check back later.</p>
         </main>
@@ -15,12 +14,13 @@
 import axios from 'axios';
 import PostsGrid from "@/components/common/PostsGrid.vue";
 import Header from "@/components/common/Header.vue";
-
+import Pagination from "@/components/pagination/Pagination.vue";
 export default {
-    components: {Header, PostsGrid},
+    components: {Header, PostsGrid, Pagination},
     data() {
         return {
             posts: [],
+            pagination: {},
         };
     },
     mounted() {
@@ -28,10 +28,12 @@ export default {
         this.fetchPosts();
     },
     methods: {
-        async fetchPosts() {
+        async fetchPosts(page = 1) {
             try {
-                const response = await axios.get('http://localhost:8000/api/posts'); // Adjust the API endpoint as needed
+                const response = await axios.get(`http://localhost:8000/api/posts?page=${page}`); // Adjust the API endpoint as needed
                 this.posts = response.data.posts.data;
+                this.pagination = response.data.posts;
+                console.log("PAGINATION=>", this.pagination);
             } catch (error) {
                 console.error('Error fetching posts:', error);
             }
