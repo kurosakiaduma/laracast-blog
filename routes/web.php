b07acb809d5aff37797527754a2090fba2427601
Tevin Aduma <tevin74@live.com>
2023-09-26 11:31:51 +0300

    transitioned web routes to api endpoints

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
-
 Route::post('newsletter', NewsletterController::class);
-
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
-
-Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
-Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
