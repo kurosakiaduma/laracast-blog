dd52e21dcc50a51210d8447f1d6f39082e626356
Tevin Aduma <tevin74@live.com>
2023-09-27 16:51:58 +0300

    feat: migrated fresh Vue scaffolding with Laravel 10
    description: Updated all references to Vue installation in the project
    with a fresh installation of Laravel 10 + Vue x Vite

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 ////Handles authenticated user requests
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
@@ -48,3 +37,8 @@ Route::get('/login', [SessionsController::class, 'create'])->middleware('guest')
 Route::post('/login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::get('/categories', [CategoryController::class, 'index']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
